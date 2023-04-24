6a2ecb116e806bac820d0d8715a36893d177a0c0
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-04-24 13:12:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const byteSize = (str) => {
   // write your code here
+	const utf8Bytes = new TextEncoder().encode(str);
+  // Return the length of the array (i.e., the size of the string in bytes)
+  return utf8Bytes.length;
 };
 
 // Do not change the code below
